e4705b43b7ea2e15ca475551632c453922403f82
Robert-Wett <rdwettlaufer@gmail.com>
2016-09-14 22:00:44 -0700

    Add client-side cache layer

--- app/index.js
+++ app/index.js
@@ -1,26 +1,99 @@
 import { h, render, Component } from 'preact';
 import { Well, PageHeader } from 'react-bootstrap';
 
+const styles = {
+	rhymeList: {
+		display: "flex",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-between",
+		marginTop: 25
+	},
+
+	rhymeEntry: {
+		strength: {
+			border: "1px solid darkgrey",
+			marginTop: 10,
+			padding: "1px 2px 1px 2px"
+		},
+
+		word: {
+			fontSize: "1.1em",
+			fontWeight: 500,
+			letterSpacing: "2px",
+			marginLeft: 10,
+			textTransform: "uppercase"
+		}
+	}
+
+};
+
 const asJson = r => r.json();
+
+const Header = () => <PageHeader>Rhyme Helper</PageHeader>;
+
+const Rhyme = ({ rhymeData }) => (
+	<Well>
+		<p>
+			<span style={{ ...styles.rhymeEntry.strength }}>
+					{ rhymeData.strength }
+			</span>
+			<span style={{ ...styles.rhymeEntry.word }}>
+					{ rhymeData.word }
+			</span>
+		</p>
+	</Well>
+);
+
+
+class Model {
+	constructor() {
+		this.cache = new Map();
+	}
+
+	_makeGetRequest(term) {
+		return new Promise(resolve => {
+			fetch(`/api/${term}`)
+				.then(asJson)
+				.then(rhymes => {
+					resolve([term, rhymes]);
+				})
+				.catch(e => {
+					resolve([term, []]);
+				});
+		});
+	}
+
+	_cacheAndReturnTermRhymes = (termArray) => {
+		this.cache.set(termArray[0], termArray[1]);
+		return Promise.resolve(termArray[1]);
+	}
+
+
+	search(term) {
+		const _term = term.toLowerCase();
+
+		if (this.cache.has(_term)) {
+			return Promise.resolve(this.cache.get(_term))
+		}
+
+		return this._makeGetRequest(_term)
+			.then(this._cacheAndReturnTermRhymes)
+	}
+}
 
 class Container extends Component {
 	state = {
 		term: 'orange',
 		loading: false,
-		rhymes: []
+		rhymes: [],
+		model: new Model()
 	};
 
 	loadRhymes = (term) => {
-		this.setState({term, loading: true});
-
-		fetch(`/api/${term}`)
-			.then(asJson)
-			.then(rhymes => {
-				this.setState({rhymes, loading: false});
-			})
-			.catch(e => {
-				this.setState({rhymes: [], loading: false})
-			});
+		this.setState({ term, loading: true });
+		this.state.model.search(term)
+			.then(rhymes => this.setState({ rhymes, loading: false }));
 	};
 
 	handleChange(e) {
@@ -39,15 +112,6 @@ class Container extends Component {
 	}
 
 	render({}, {term}={}){
-		const rhymes = this.state.rhymes.map(r => {
-			return (
-				<Well>
-					<p>{r.word}</p>
-					<p># of phenomes matched: {r.strength}</p>
-				</Well>
-			);
-		});
-
 		return (
 			<div className="container">
 				<Header />
@@ -57,14 +121,12 @@ class Container extends Component {
 					value={ term }
 					onChange={::this.handleChange}
 				/>
-				<div className="rhymeList">
-					{rhymes}
+				<div style={{ ...styles.rhymeList }}>
+					{ this.state.rhymes.slice(0, 50).map(r => <Rhyme rhymeData={r} />) }
 				</div>
 			</div>
 		)
 	}
 }
-
-const Header = () => <PageHeader>Rhyme Helper</PageHeader>;
 
 render(<Container />, document.body);
